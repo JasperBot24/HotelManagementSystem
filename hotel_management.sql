9ba0f3a4e240ef252e30aebb1cdab38148112df3
Jasper Boticario <145410041+JasperBot24@users.noreply.github.com>
2025-05-07 18:44:13 +0800

    Add files via upload

--- hotel_management.sql
+++ hotel_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2025 at 02:43 AM
+-- Generation Time: May 07, 2025 at 12:39 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -50,10 +50,14 @@ INSERT INTO `checkouts` (`checkout_id`, `reservation_id`, `customer_id`, `room_n
 (35, 57, '001', '101', '2025-03-29 16:01:38', '2025-03-29 16:02:52', 4499.00, 'Checked-out'),
 (36, 59, 'W05', '106', '2025-03-29 16:02:25', '2025-04-06 12:31:59', 10493.00, 'Checked-out'),
 (37, 60, '001', '105', '2025-03-29 16:15:15', '2025-03-29 16:17:18', 1999.00, 'Checked-out'),
-(38, 61, '001', '108', '2025-03-29 16:15:15', '2025-04-06 12:31:59', 31493.00, 'Checked-out'),
-(45, 69, '006', '105', '2025-04-12 00:00:00', '2025-04-13 00:00:00', 1999.00, 'Checked-in'),
-(46, 70, 'W08', '102', '2025-04-12 21:04:17', '2025-04-13 21:04:20', 5499.00, 'Checked-in'),
-(47, 71, '006', '202', '2025-04-12 00:00:00', '2025-04-15 00:00:00', 5499.00, 'Checked-in');
+(38, 61, '001', '108', '2025-03-29 16:15:15', '2025-05-07 13:13:30', 4499.00, 'Checked-out'),
+(45, 69, '006', '105', '2025-04-12 00:00:00', '2025-05-05 11:03:56', 47976.00, 'Checked-out'),
+(46, 70, 'W08', '102', '2025-04-12 21:04:17', '2025-05-05 11:04:02', 126477.00, 'Checked-out'),
+(47, 71, '006', '202', '2025-04-12 00:00:00', '2025-05-05 11:03:52', 131976.00, 'Checked-out'),
+(48, 72, '001', '201', '2025-05-05 00:00:00', '2025-05-07 08:05:10', 10497.00, 'Checked-out'),
+(49, 73, '010', '106', '2025-05-07 00:00:00', '2025-05-07 08:15:29', 1499.00, 'Checked-out'),
+(50, 74, 'W09', '102', '2025-05-07 08:07:21', '2025-05-10 00:00:00', 10998.00, 'Checked-in'),
+(51, 75, '001', '108', '2025-05-07 00:00:00', '2025-05-07 13:13:30', 4499.00, 'Checked-out');
 
 -- --------------------------------------------------------
 
@@ -91,7 +95,13 @@ INSERT INTO `check_history` (`history_id`, `customer_id`, `customer_name`, `phon
 (38, '001', 'Aaron Perez', '09123456777', 'aaron@gmail.com', 'bagumbong caloocan city', '105', 'Double Room', '4', 1999.00, '2025-03-29 16:15:15', '2025-03-29 16:17:18', 1, 1999.00, '2025-03-29 08:17:19'),
 (39, '001', 'Aaron Perez', '09123456777', 'aaron@gmail.com', 'bagumbong caloocan city', '102', 'Suite', '8', 5499.00, '2025-03-29 16:01:38', '2025-04-06 12:31:59', 7, 38493.00, '2025-04-06 04:31:59'),
 (40, 'W05', 'asdasd', '09999111111', NULL, 'cascasdas', '106', 'Single Room', '2', 1499.00, '2025-03-29 16:02:25', '2025-04-06 12:31:59', 7, 10493.00, '2025-04-06 04:31:59'),
-(41, '001', 'Aaron Perez', '09123456777', 'aaron@gmail.com', 'bagumbong caloocan city', '108', 'Deluxe Room', '6', 4499.00, '2025-03-29 16:15:15', '2025-04-06 12:31:59', 7, 31493.00, '2025-04-06 04:31:59');
+(41, '001', 'Aaron Perez', '09123456777', 'aaron@gmail.com', 'bagumbong caloocan city', '108', 'Deluxe Room', '6', 4499.00, '2025-03-29 16:15:15', '2025-04-06 12:31:59', 7, 31493.00, '2025-04-06 04:31:59'),
+(47, '006', 'JM Boticario', '09281234567', 'jm11@gmail.com', 'Camarin Caloocan City', '202', 'Suite', '8', 5499.00, '2025-04-12 00:00:00', '2025-05-05 11:03:52', 24, 131976.00, '2025-05-05 03:03:52'),
+(48, '006', 'JM Boticario', '09281234567', 'jm11@gmail.com', 'Camarin Caloocan City', '105', 'Double Room', '4', 1999.00, '2025-04-12 00:00:00', '2025-05-05 11:03:56', 24, 47976.00, '2025-05-05 03:03:57'),
+(49, 'W08', 'LeBron', '09281334789', '', 'Novaliches, Quezon City', '102', 'Suite', '8', 5499.00, '2025-04-12 21:04:17', '2025-05-05 11:04:02', 23, 126477.00, '2025-05-05 03:04:03'),
+(50, '001', 'Aaron Perez', '09123456777', 'aaron@gmail.com', 'bagumbong caloocan city', '201', 'Queen Room', '6', 3499.00, '2025-05-05 00:00:00', '2025-05-07 08:05:10', 3, 10497.00, '2025-05-07 00:05:18'),
+(51, '010', 'Junnieper', '09928337746', 'junnie@gmail.com', 'kaligayahan nova qc', '106', 'Single Room', '2', 1499.00, '2025-05-07 00:00:00', '2025-05-07 08:15:29', 1, 1499.00, '2025-05-07 00:15:30'),
+(52, '001', 'Aaron Perez', '09123456777', 'aaron@gmail.com', 'bagumbong caloocan city', '108', 'Deluxe Room', '6', 4499.00, '2025-05-07 00:00:00', '2025-05-07 13:13:30', 1, 4499.00, '2025-05-07 05:13:36');
 
 -- --------------------------------------------------------
 
@@ -125,7 +135,11 @@ INSERT INTO `check_ins` (`check_in_id`, `reservation_id`, `customer_id`, `custom
 (20, 61, '001', 'Aaron Perez', '09123456777', '108', '2025-03-29 16:15:15', 'checked-in'),
 (27, 69, '006', 'JM Boticario', '09281234567', '105', '2025-04-12 00:00:00', 'checked-in'),
 (28, 70, 'W08', 'LeBron', '09281334789', '102', '2025-04-12 21:04:17', 'checked-in'),
-(29, 71, '006', 'JM Boticario', '09281234567', '202', '2025-04-12 00:00:00', 'checked-in');
+(29, 71, '006', 'JM Boticario', '09281234567', '202', '2025-04-12 00:00:00', 'checked-in'),
+(30, 72, '001', 'Aaron Perez', '09123456777', '201', '2025-05-05 00:00:00', 'checked-in'),
+(31, 73, '010', 'Junnieper', '09928337746', '106', '2025-05-07 00:00:00', 'checked-in'),
+(32, 74, 'W09', 'Junnieee', '09113333444', '102', '2025-05-07 08:07:21', 'checked-in'),
+(33, 75, '001', 'Aaron Perez', '09123456777', '108', '2025-05-07 00:00:00', 'checked-in');
 
 -- --------------------------------------------------------
 
@@ -147,19 +161,23 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `customer_fullname`, `customer_email`, `customer_phoneNumber`, `customer_address`, `customer_password`) VALUES
-('001', 'Aaron Perez', 'aaron@gmail.com', '09123456777', 'bagumbong caloocan city', '123'),
+('001', 'Aaron Perez', 'aaron@gmail.com', '09123456777', 'bagumbong caloocan city', '12345678'),
 ('002', 'Jhonalyn Perez', 'jho@gmail.com', '09123456788', 'llano, caloocan city', '123'),
 ('003', 'Ethan Perez', 'ethan123@gmail.com', '09992222222', 'bagumbong caloocan city', 'ethan123'),
 ('004', 'Kierance Perez', 'kier@gmail.com', '09912121212', 'quezon city', 'kier123'),
 ('006', 'JM Boticario', 'jm11@gmail.com', '09281234567', 'Camarin Caloocan City', 'jm@11'),
 ('007', 'James Boticario', 'james11@gmail.com', '09223456789', 'Deparo, Caloocan City', 'james@11'),
+('008', 'Janella', 'janella26@gmail.com', '09228122123', 'Camarin, Caloocan City', '123123'),
+('009', 'Janssen', 'janssen19@gmail.com', '09156778838', 'Novaliches, Quezon City', '123'),
+('010', 'Junnieper', 'junnie@gmail.com', '09928337746', 'kaligayahan nova qc', '123456'),
 ('W01', 'kiier', NULL, '09123456789', 'caloocanm', NULL),
 ('W02', 'ethan', NULL, '09912345678', 'caloocan', NULL),
 ('W03', 'kier', NULL, '09191919191', 'bagumbong', NULL),
 ('W05', 'asdasd', NULL, '09999111111', 'cascasdas', NULL),
 ('W06', 'asdasdasd', NULL, '09912121212', 'calooasd', NULL),
 ('W07', 'asdasd', NULL, '09121212121', 'asdasd', NULL),
-('W08', 'LeBron', NULL, '09281334789', 'Novaliches, Quezon City', NULL);
+('W08', 'LeBron', NULL, '09281334789', 'Novaliches, Quezon City', NULL),
+('W09', 'Junnieee', NULL, '09113333444', 'nova qc', NULL);
 
 -- --------------------------------------------------------
 
@@ -182,7 +200,8 @@ CREATE TABLE `deleted_rooms` (
 --
 
 INSERT INTO `deleted_rooms` (`room_number`, `room_type`, `capacity`, `price`, `status`, `date_added`, `deleted_at`) VALUES
-('203', 'Single Room', 2, 1499, 'Available', '2025-04-15', '2025-04-15 00:41:20');
+('203', 'Single Room', 2, 1499, 'Available', '2025-04-15', '2025-04-15 00:41:20'),
+('110', 'Deluxe Room', 6, 4499, 'Available', '2025-03-19', '2025-05-07 00:03:57');
 
 -- --------------------------------------------------------
 
@@ -206,11 +225,12 @@ CREATE TABLE `employees` (
 --
 
 INSERT INTO `employees` (`employee_id`, `employee_fullname`, `employee_phoneNumber`, `employee_email`, `employee_address`, `employee_password`, `employee_role`, `default_salary`) VALUES
-('1', 'Jasper', '09123456333', 'manager@gmail.com', 'Hilltop, Lagro Quezon City', '1234', 'Manager', 35000),
-('2', 'Aaron Sean', '09123456789', 'admin@gmail.com', '123 Admin St', 'admin123', 'Admin', 40000),
+('1', 'Jasper', '09123456333', 'manager@gmail.com', 'Hilltop, Lagro Quezon City', '123456', 'Manager', 35000),
 ('4', 'Sean', '09991212123', 'sean123@gmail.com', 'caloocan', '123', 'Manager', 35000),
 ('5', 'asdasd', '09999999999', 'asd@gmail.com', 'asdasd', 'asd', 'Stock', 15000),
-('6', 'asd', '09900000000', 'aaron1@gmail.com', 'asdasd', '123', 'Maintenance', 20000);
+('6', 'asd', '09900000000', 'aaron1@gmail.com', 'asdasd', '123', 'Maintenance', 20000),
+('7', 'admin', '09121234567', 'admin@gmail.com', 'Hilltop Lagro Quezon City', 'admin123', 'Admin', 40000),
+('8', 'superadmin', '09228812345', 'superadmin@gmail.com', 'Lagro Quezon City', 'superadmin', 'Admin', 40000);
 
 -- --------------------------------------------------------
 
@@ -292,7 +312,11 @@ INSERT INTO `reservations` (`reservation_id`, `customer_id`, `room_number`, `che
 (61, '001', '108', '2025-03-29 16:15:15', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-03-31 16:15:16'),
 (69, '006', '105', '2025-04-12 00:00:00', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-04-13 00:00:00'),
 (70, 'W08', '102', '2025-04-12 21:04:17', 'Walk-in', NULL, NULL, NULL, NULL, NULL, '2025-04-13 21:04:20'),
-(71, '006', '202', '2025-04-12 00:00:00', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-04-15 00:00:00');
+(71, '006', '202', '2025-04-12 00:00:00', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-04-15 00:00:00'),
+(72, '001', '201', '2025-05-05 00:00:00', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-05-07 00:00:00'),
+(73, '010', '106', '2025-05-07 00:00:00', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-05-08 00:00:00'),
+(74, 'W09', '102', '2025-05-07 08:07:21', 'Walk-in', NULL, NULL, NULL, NULL, NULL, '2025-05-08 08:07:25'),
+(75, '001', '108', '2025-05-07 00:00:00', 'Approved', NULL, NULL, NULL, NULL, NULL, '2025-05-08 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -316,11 +340,13 @@ CREATE TABLE `rooms` (
 INSERT INTO `rooms` (`room_number`, `room_type`, `capacity`, `price`, `status`, `date_added`) VALUES
 ('101', 'Deluxe Room', 6, 4499.00, 'Not Available', '2025-03-19'),
 ('102', 'Suite', 8, 5499.00, 'Occupied', '2025-03-18'),
-('105', 'Double Room', 4, 1999.00, 'Occupied', '2025-03-18'),
-('106', 'Single Room', 2, 1499.00, 'Available', '2025-03-19'),
+('105', 'Double Room', 4, 1999.00, 'Not Available', '2025-03-18'),
+('106', 'Single Room', 2, 1499.00, 'Not Available', '2025-03-19'),
 ('108', 'Deluxe Room', 6, 4499.00, 'Not Available', '2025-03-19'),
-('201', 'Queen Room', 6, 3499.00, 'Available', '2025-04-07'),
-('202', 'Suite', 8, 5499.00, 'Occupied', '2025-04-09');
+('110', 'Deluxe Room', 6, 4499.00, 'Available', '2025-05-07'),
+('201', 'Queen Room', 6, 3499.00, 'Not Available', '2025-04-07'),
+('202', 'Suite', 8, 5499.00, 'Available', '2025-04-09'),
+('204', 'Deluxe Room', 6, 4499.00, 'Available', '2025-05-06');
 
 --
 -- Indexes for dumped tables
@@ -385,25 +411,25 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `checkouts`
 --
 ALTER TABLE `checkouts`
-  MODIFY `checkout_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `checkout_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `check_history`
 --
 ALTER TABLE `check_history`
-  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT for table `check_ins`
 --
 ALTER TABLE `check_ins`
-  MODIFY `check_in_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `check_in_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- Constraints for dumped tables
